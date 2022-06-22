21f49010755b33bc222e771a2cb13943ef13a350
robel baye <robelbaye6@gmail.com>
2022-06-22 12:16:47 +0300

    react setup

---
+++ resources/js/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Table from './employeeList/Table';
+
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+               {/* components go here */}
+               <Table/>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
